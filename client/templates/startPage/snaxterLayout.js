80af4cf8f2416bafe859232413add4d018f0a657
ScyDev <lukas.saegesser@gmail.com>
2016-03-14 15:44:54 +0100

    - use proper template names and replacing works

--- client/templates/startPage/snaxterLayout.js
+++ client/templates/startPage/snaxterLayout.js
@@ -1,12 +1,18 @@
-
+/*
 Template.snaxterHead.inheritsHelpersFrom(["layoutHead"]);
 Template.snaxterHead.inheritsEventsFrom(["layoutHead"]);
 Template.snaxterHead.inheritsHooksFrom(["layoutHead"]);
-
-Template.snaxterHead.replaces("layoutHead");
-
+*/
+Template.snaxterHead.replaces("coreHead");
+/*
 Template.snaxterLayout.inheritsHelpersFrom(["layoutCore"]);
 Template.snaxterLayout.inheritsEventsFrom(["layoutCore"]);
 Template.snaxterLayout.inheritsHooksFrom(["layoutCore"]);
+*/
+Template.snaxterLayout.replaces("coreLayout");
+console.log("ello?");
 
-Template.snaxterLayout.replaces("layoutCore");
+Template.snaxterLayout.onRendered(function(){
+    console.log("template 'layout' rendered!");
+  }
+);
